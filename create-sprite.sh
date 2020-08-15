3fc220e2b62cd11b3b0992b94ca7e22f92814dd1
Jin IGARASHI <wushilan0623@gmail.com>
2020-08-15 14:38:09 +0900

    updated for nakuru water

--- create-sprite.sh
+++ create-sprite.sh
@@ -1,7 +1,7 @@
 mkdir icons
 cd icons
 git clone https://github.com/mapbox/maki.git
-git clone https://github.com/narwassco/water-icons.git
+git clone https://github.com/nakuruwater/water-icons.git
 git clone https://github.com/narwassco/mapbox-street-icons.git
 git clone https://github.com/narwassco/mapbox-satellite-icons.git
 cd ..
